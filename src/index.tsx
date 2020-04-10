14ea2d599bb22e285ae9ea45091736a1e1f779a6
jeg <jugheadeatsalot@gmail.com>
2020-04-10 03:31:57 -0700

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,6 @@ import { AppProvider as PolarisProvider } from '@shopify/polaris';
 import App from './components/App';
 import { createToastLayer } from 'utils/createToastLayer';
 
-import './App.modules.css';
-
 // If not in development, kick off production specific DOM mutations.
 if (API_URL !== 'http://localhost:7777') {
   clearDom();
