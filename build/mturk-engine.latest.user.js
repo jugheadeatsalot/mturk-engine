14930195f0557e13f6b732eb3d761fe53511a2c1
jeg <jugheadeatsalot@gmail.com>
2020-04-12 01:56:57 -0700

    0.0.2-dev.14

--- build/mturk-engine.latest.user.js
+++ build/mturk-engine.latest.user.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name         Mturk Engine Rover
 // @namespace    https://github.com/Anveio/mturk-engine/
-// @version      0.0.2-dev.13
+// @version      0.0.2-dev.14
 // @description  Earn money more efficiently on Amazon's Mechanical Turk work platform.
 // @author       Anveio (Shovon Hasan)
 // @license      MIT
 // @match        https://worker.mturk.com/?mturkengine
 // @match        https://www.mturk.com/?mturkengine
-// @require      https://cdn.jsdelivr.net/gh/jugheadeatsalot/mturk-engine@0.0.2-dev.13/build/mturk-engine.latest.raw.js?raw=true
-// @grant        none
+// @require      https://cdn.jsdelivr.net/gh/jugheadeatsalot/mturk-engine@0.0.2-dev.14/build/mturk-engine.latest.raw.js?raw=true
+// @grant        GM_addStyle
 // ==/UserScript==
+
+GM_addStyle(.Polaris-Select__SelectedOption{display:none}.blockedRequesterTag{display:flex;align-items:center}.blockedRequesterTag .Polaris-Tag{border-radius:0}.blockedRequesterTag .Polaris-Tag:last-child{padding-left:0}.blockedRequesterTag .Polaris-Tag__TagText{display:none});
 
 /**
  * After downloading this script visit https://worker.mturk.com/?mturkengine
